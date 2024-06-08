9e0ff703c56c0579675898a6a416ffd90a80fab4
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-07 21:18:56 -0500

    up to insertAt completed

--- linked-list.js
+++ linked-list.js
@@ -21,59 +21,136 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
-    let newNode = new Node.val(val);
+    let newNode = new Node(val);
     if (this.head === null) this.head = newNode;
-    if (this.tail !== null) this.tail.next=newNode;
+    if (this.tail !== null) this.tail.next = newNode;
     this.tail = newNode;
+    return undefined;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    return undefined;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    if (!this.head) {
+      throw new Error("Can't pop from an empty list.");
+    }
+    let current = this.head;
+    let newTail = current;
 
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current.val;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    if (!this.head) {
+      throw new Error("List is empty!");
+    }
+    let oldHead = this.head;
 
+    this.head = oldHead.next;
+
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return oldHead.val;
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index is out of bounds.");
+    }
+    let current = this.head;
+    let count = 0;
 
+    while (count != idx) {
+      current = current.next;
+      count++;
+    }
+    return current.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index is out of bounds!")
+    }
+    let current = this.head;
+    let count = 0;
+    while (count != idx) {
+      current = current.next;
+      count++;
+    }
+    current.val = val;
+    return this;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    let newNode = new Node(val);
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index is invalid!");
+    } 
+    if (idx === 0) {
+      return this.unshift(newNode);
+    }
+    if (idx === this.length) {
+      return this.push(newNode);
+    }
+    let current = this.head;
+    let count = 0;
+    while (count != idx -1){
+      current = current.next;
+      count ++
+    }
+    newNode.next = current.next;
+    current.next = newNode;
+    this.length++;
+    return this;
+    }
 
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
-  removeAt(idx) {
-
-  }
+  removeAt(idx) {}
 
   /** average(): return an average of all values in the list */
 
-  average() {
-    
-  }
+  average() {}
 }
 
 module.exports = LinkedList;
